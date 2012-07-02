518100c3dc4ca42b47eb2565249258514255f6a7
John Allen <john@threedogconsulting.com>
2012-07-01 22:49:42 -0400

    removes ember from production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 ScheduleLiberator::Application.configure do
-  config.ember.variant = :production
+  # config.ember.variant = :production
 
   # Settings specified here will take precedence over those in config/application.rb
 
