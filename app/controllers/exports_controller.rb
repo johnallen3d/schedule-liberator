df025edf529afd43b685b571568184e31b07c050
John Allen <john@threedogconsulting.com>
2012-07-21 20:49:47 -0400

    adds file extension for export

--- app/controllers/exports_controller.rb
+++ app/controllers/exports_controller.rb
@@ -4,10 +4,18 @@ class ExportsController < ApplicationController
       schedule = ScheduleScraper.fetch(params[:url])
       format = params[:to].to_sym
 
-      send_data schedule.send("to_#{format}")
+      send_data schedule.send("to_#{format}"), :filename => filename
     rescue Exception => e
       logger.info e
       render :nothing => true
     end
+  end
+
+  private
+
+  def filename
+    extension = params[:to] == "ical" ? "ics" : "csv"
+
+    "export.#{extension}"
   end
 end
