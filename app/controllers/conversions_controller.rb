a7881dc47fcc6e94d17e5d42fed70b49f0711b32
John Allen <john@threedogconsulting.com>
2014-02-09 10:27:19 -0500

    Adds retry when failing to parse schedule

--- app/controllers/conversions_controller.rb
+++ app/controllers/conversions_controller.rb
@@ -1,12 +1,24 @@
 class ConversionsController < ApplicationController
   def create
+    retries = 0
+
     begin
       schedule = ScheduleScraper.fetch(params[:url])
 
       render :json => { :schedule => schedule.to_h }
     rescue Exception => e
-      logger.info e
-      render :nothing => true
+      retries = retries + 1
+
+      if retries < 5
+        logger.info "error - retry: #{retries}"
+        logger.info e
+
+        sleep 0.25
+        retry
+      else
+        logger.info "error - retry max reached"
+        render :nothing => true
+      end
     end
   end
 end
